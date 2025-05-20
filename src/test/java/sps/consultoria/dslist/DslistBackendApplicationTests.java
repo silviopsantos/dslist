43ff0711d626442689ec5c5551f52af693af1fee
Silvio Santos <62785094+silviopsantos@users.noreply.github.com>
2025-05-19 21:20:35 -0300

    Projeto Criado

---
+++ src/test/java/sps/consultoria/dslist/DslistBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package sps.consultoria.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
